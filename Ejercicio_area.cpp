8963b73cc8789beb7b8b0a89355aa5b0d536f454
Dina Milagros Delgado Quispe <75899918@est.unap.edu.pe>
2026-01-26 08:50:15 -0500

    'ver2'

---
+++ Ejercicio_area.cpp
@@ -0,0 +1,12 @@
+#include <iostream>   // Librería para entrada/salida
+using namespace std;  // Para usar cout y cin sin problemas
+
+int main(int argc, char** argv) {
+   float lado, area;  // Aquí va el código
+    cout <<"Ingrese el lado del cuadrado" <<endl;
+	cin >> lado,
+	area=lado*lado; 
+	cout <<"area = " << area << endl;
+	cout << "El area es: " <<area;
+}
+
